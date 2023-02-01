21e65aafa93db9e8e12e71192708c7bf13373895
BAHIRU YIMOLAL <88880193+Bahiru-Yimolal@users.noreply.github.com>
2023-02-01 22:17:18 +0300

    Create SelectionSort.py

---
+++ Week-one/SelectionSort.py
@@ -0,0 +1,10 @@
+class Solution: 
+    # def select(self, arr, i):
+    #     # code here 
+
+    def selectionSort(self, arr,n):
+        #code here
+        for n in range(len(arr)):
+            arr.sort()
+
+        return arr
